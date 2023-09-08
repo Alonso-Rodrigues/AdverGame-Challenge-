086abc26c30d226fb61fbac93572fd3fbe1fcde3
unknown <fakajandi@gmail.com>
2023-09-08 11:59:21 +0200

    titre rouge

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        h1{
+            color:red;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
