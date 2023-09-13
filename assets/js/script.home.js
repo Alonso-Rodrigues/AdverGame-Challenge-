832002cf2272d8b259904bd7f3ac8a37e5540927
andi <fakajandi@gmail.com>
2023-09-13 14:57:10 +0200

    new files

---
+++ assets/js/script.home.js
@@ -0,0 +1,70 @@
+const player = document.querySelector('.player');
+const obstacle = document.querySelector('.obstacle');
+const gameBoard = document.querySelector('.game-board');
+const btn = document.querySelector('.restart')
+
+const jump = () => {
+
+    player.classList.add('jump');
+
+    setTimeout(() => {
+        player.classList.remove('jump');
+    }, 500);
+}
+
+let life = 2;
+
+btn.addEventListener('click', () => {
+    start();
+})
+
+const endGame = () => {
+
+    gameBoard.classList.add('gameOver');
+
+    setTimeout(() => {
+        window.location = "http://advergame/pages/form.php";
+    }, 3000);
+}
+
+function start() {
+
+    document.addEventListener('keydown', jump);
+
+    obstacle.classList.remove("obstacle");
+    obstacle.classList.add("obstacle");
+    obstacle.classList.remove("hidden");
+
+    player.src = "../assets/img/player.gif";
+    btn.classList.add("hidden");
+
+    const loop = setInterval(() => {
+        const obstaclePosition = obstacle.offsetLeft;
+        const playerPosition = +window.getComputedStyle(player).bottom.replace('px', '');
+
+        if (obstaclePosition <= 50 && obstaclePosition > 0 && playerPosition < 137) {
+            life--;
+
+            setTimeout(() => {
+
+                obstacle.classList.add("hidden");
+            }, 500);
+
+            clearInterval(loop);
+            document.removeEventListener('keydown', jump);
+
+            //player.style.animation = 'none';
+            //player.style.bottom = `${playerPosition + 10}px`;
+            player.src = "../assets/img/sad.gif";
+
+            if (life <= 0) {
+                endGame();
+            } else {
+                btn.classList.remove("hidden");
+            }
+        }
+    }, 10);
+}
+
+start();
+
---
+++ pages/form.php
@@ -0,0 +1,82 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"]."../connection/connect.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Verifique se os campos do formulário foram definidos antes de acessá-los
+    if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['phone'])) {
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+
+        $regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/'; 
+        if (!preg_match($regex, $_POST['email'])) {
+        echo $email . "Attention, l'email n'est pas valide";
+
+        }else {
+
+            // Préparer la statement
+            $stmt = $db->prepare("INSERT INTO user (nom, prenom, email, phone) VALUES (:nom, :prenom, :email, :phone)");
+            
+            // Exécuter statement  
+            if($stmt->execute([':nom' => $nom, 
+            ':prenom' => $prenom,
+            ':email' => $email, 
+            ':phone' => $phone
+            ])) {
+                echo "Enregistrement réussi.";
+                header("Location: game.html");
+            } else {
+                echo "Erreur lors de l'enregistrement.";
+            } 
+        }
+
+    } else {
+        echo "Veuillez remplir tous les champs du formulaire.";
+    }  
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.form.css">
+    <title>Form Adver Game</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Quitter le jeu</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <div class="container">
+                <h3>Remplissez le formulaire pour rejouer</h3>
+                <form method="post" action="form.php">
+                    <label for="nom">Nom</label>
+                    <input type="text" name="nom">
+                    <label for="prenom">Prénom</label>
+                    <input type="text" name="prenom">
+                    <label for="email">Email</label>
+                    <input type="text" name="email">
+                    <label for="phone">Phone</label>
+                    <input type="text" name="phone">
+                    <button type="submit">Rejouer</button>
+                </form>
+            </div>
+        </section>
+    </main>
+    <footer>
+        All rights reserved to Alonso Rodrigues and Kostandin Fakaj
+    </footer>
+</body>
+
+</html>
