a7f216ba998411f14ba623dc7d5398121aa87d2a
Alonso <alonso.rbass@gmail.com>
2023-09-18 10:53:24 +0200

    ok

--- pages/game.php
+++ pages/game.php
@@ -1,7 +1,3 @@
-<?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "../traitement/traitement.php";
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,15 +10,19 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "../traitement/traitement.php";
 
 <body>
     <main class="game-board">
+
         <div class="score"></div>
+
         <div class="lifeCounter"></div>
+
         <div class="timming"></div>
         <button class="restart hidden">Rejouer</button>
         <img class="clounds" src="../assets/img/clouds.png" alt="clounds">
         <img class="player" src="../assets/img/player.gif" alt="player">
         <img class="obstacle" src="../assets/img/obstacle.png" alt="obstacle">
     </main>
-    <script src="../assets/js/script.game.js"></script>
+    <script src="../assets/js/script.home.js"></script>
+
 </body>
 
 </htm>
